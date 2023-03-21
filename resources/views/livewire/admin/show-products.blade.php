8369a77fd912959258600aefaae3327333ca948d
ruben <perezrubentrabajos@gmail.com>
2023-03-21 18:36:31 +0100

    Inicio examen.

--- resources/views/livewire/admin/show-products.blade.php
+++ resources/views/livewire/admin/show-products.blade.php
@@ -39,10 +39,6 @@
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Precio
                     </th>
-                    <th scope="col"
-                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Precio
-                    </th>
                 </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
